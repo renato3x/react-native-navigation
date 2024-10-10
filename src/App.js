dca183d7be593bb8e60434b44a177b250188706d
renato3x <pereirarenato21@gmail.com>
2024-10-09 21:48:59 -0300

    add: SafeAreaView

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <StatusBar style="dark"/>
+        <Text>This is a text in safe area</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
